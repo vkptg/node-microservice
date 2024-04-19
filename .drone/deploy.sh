5774152f56fd39fcf3ccd942fcdacb07b64f70ee
vkptg <111045894+vkptg@users.noreply.github.com>
2024-04-19 15:02:54 +0900

    Update deploy.sh

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-set -e
-
-docker rm -f nazmul_node_micro
-docker rmi nazmulb/node-microservice
-docker pull nazmulb/node-microservice
-docker run --rm -d -p 7777:3000 --name nazmul_node_micro nazmulb/node-microservice
+	set -e
+	docker pull vickydk/node-microservice
+	docker run --rm -d -p 7777:3000 --name vickydk_node_micro vickydk/node-microservice
